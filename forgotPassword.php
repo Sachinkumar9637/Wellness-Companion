05b0c778274c15054f4e264b04e1154fb187c6cc
Meet2403 <meetkadiya143@gmail.com>
2024-06-04 21:59:29 -0400

    forgotpassword.php file push

---
+++ forgotPassword.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Forgot Password</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+    <link href='https://fonts.googleapis.com/css?family=Kaisei Opti' rel='stylesheet'>
+</head>
+<body>
+	<div class="fpassword-container">
+        <div class="fpassword-image">
+            <img src="Images/home.png">
+        </div>
+        <div class="fpassword-text">
+            <h1>Welcome to Cure Corner</h1>
+            <div>
+                <form class="fpassword-form">
+                	<h3>Enter your email and we'll send you link to reset your password</h3>
+
+                    <input type="email" name="email" placeholder="Email"><br>
+                    <input type="submit" name="submit" value="Submit">
+                </form>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
