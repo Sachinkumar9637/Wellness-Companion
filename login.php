5189b61946af13e3eb8030d7b1dcf750b2c0ac43
Meet2403 <meetkadiya143@gmail.com>
2024-06-04 21:58:08 -0400

    login.php file push

---
+++ login.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Login Page</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <link href='https://fonts.googleapis.com/css?family=Kaisei Opti' rel='stylesheet'>
+</head>
+<body>
+    <div class="login-container">
+        <div class="login-image">
+            <img src="Images/home.png">
+        </div>
+        <div class="login-text">
+            <h1>Welcome to Cure Corner</h1>
+            <div>
+                <form class="login-form">
+                    <h3>Log In Into Your Account</h3>
+
+                    <input type="email" name="email" placeholder="Email"><br>
+                    <input type="password" name="password" placeholder="Password"><br>
+                    <a href="forgotPassword.php">Forgot Password?</a><br>
+                    <input type="submit" name="submit" value="LogIn">
+                </form>
+            </div>
+
+            <h2>Don't have an account? <a href="signup.html">Register Here</a></h2>
+        </div>
+    </div>
+</body>
+</html>
