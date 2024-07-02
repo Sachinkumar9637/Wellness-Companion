4104978d2097a8d2c21bf1a1a855a39240417c25
Meet2403 <meetkadiya143@gmail.com>
2024-07-01 21:39:15 -0400

    Validation Added in login.php

--- login.php
+++ login.php
@@ -1,3 +1,36 @@
+<?php 
+    session_start();
+    include("dbinit.php");
+    $error="";
+    $msg="";
+
+    if(isset($_REQUEST['login']))
+    {
+        $email=$_REQUEST['email'];
+        $password=$_REQUEST['password'];
+
+        $password= sha1($password);
+        
+        if(!empty($email) && !empty($password))
+        {
+            $sql = "SELECT * FROM register where email='$email' && password='$password'";
+            $result=mysqli_query($dbc, $sql);
+            $row=mysqli_fetch_array($result);
+               if($row){
+                    $_SESSION['ID']=$row['ID'];
+                    $_SESSION['email']=$email;
+                    header("location:index.php");
+               }
+               else{
+                   $error = "<p class='alert alert-warning'>Email or Password doesnot match!</p> ";
+               }
+        }else{
+            $error = "<p class='alert alert-warning'>Please Fill all the fields</p>";
+        }
+    }
+?>
+
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -6,6 +39,7 @@
     <title>Login Page</title>
     <link rel="stylesheet" type="text/css" href="style.css">
     <link href='https://fonts.googleapis.com/css?family=Kaisei Opti' rel='stylesheet'>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
     <?php include 'header.php';?>
@@ -14,19 +48,21 @@
             <img src="Images/home.png">
         </div>
         <div class="login-text">
-            <h1>Welcome to Cure Corner</h1>
+            <h1>Welcome to Cure Corner</h1><br>
             <div>
-                <form class="login-form">
+                <form class="login-form" method="post">
                     <h3>Log In Into Your Account</h3>
 
+                    <?php echo $error; ?><?php echo $msg; ?>
                     <input type="email" name="email" placeholder="Email"><br>
                     <input type="password" name="password" placeholder="Password"><br>
                     <a href="forgotPassword.php">Forgot Password?</a><br>
-                    <input type="submit" name="submit" value="LogIn">
-                </form>
-            </div>
 
-            <h2>Don't have an account? <a href="signup.html">Register Here</a></h2>
+                    <button type="submit" name="login">LogIn</button>
+                </form>
+            </div><br>
+
+            <h3>Don't have an account? <a href="signup.php">Register Here</a></h3>
         </div>
     </div>
     <?php include 'footer.php'; ?>
