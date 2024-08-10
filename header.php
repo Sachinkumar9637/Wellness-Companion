519b6cdf9c90d5dc8c2a83eca2b7463a8e976c1c
Meet2403 <meetkadiya143@gmail.com>
2024-08-09 20:28:03 -0400

    Updated Header.php

--- header.php
+++ header.php
@@ -33,7 +33,10 @@
                 <ul>
                     <li><a href="index.php">Home</a></li>
                     <li><a href="aboutus.php">About Us</a></li>
-                    <li><a href="services.php">Services</a></li>
+                    <li><a href="lowBackPain.php">Low BackPain</a></li>
+                    <li><a href="neckPain.php">Neck Pain</a></li>
+                    <li><a href="shoulderPain.php">Shoulder Pain</a></li>
+                    <li><a href="kneePain.php">Knee Pain</a></li>
                     <li><a href="healingStories.php">Healing Stories</a></li>
                     <li><a href="#">How it works</a></li>
                     <?php
